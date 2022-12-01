7e77b2dfe740fbe808c3182b6cddd17173cee682
chardbres <richard.m.bresnahan@gmail.com>
2022-12-01 14:27:24 -0500

    Add theme scss files and attempt to link with button component.

--- sport-app/shared/button/button.composition.tsx
+++ sport-app/shared/button/button.composition.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { Button } from './button';
 
 export const BasicButton = () => {
-  return <Button label={'Primay'} />;
+  return <Button label={'Primary'} />;
 };
