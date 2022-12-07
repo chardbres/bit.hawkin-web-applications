90b596fd2a2520d2580d2b59a0b06f74ede6a86a
chardbres <richard.m.bresnahan@gmail.com>
2022-12-07 11:37:45 -0500

    Add Jest testing library and write basic render test for button.

--- sport-app/shared/button/button.spec.tsx
+++ sport-app/shared/button/button.spec.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Button } from './button';
 
-it('should render with the correct text', () => {
-  const { getByText } = render(<Button type={'primary'} label={'label'} />);
-  const rendered = getByText('hello world!');
-  expect(rendered).toBeTruthy();
+it('should render a button with the class of primary', () => {
+  render(<Button type={'primary'} label={'Primary'} />);
+  const primaryButton = screen.getByRole('button', { name: /primary/i });
+  expect(primaryButton).toHaveClass('primary');
 });
