fa993a14d14f30ad8b8539228410c86003b3edb3
chardbres <richard.m.bresnahan@gmail.com>
2022-12-08 13:40:35 -0500

    Incorporate jest-axe and confirm successful a11y test on button.

--- sport-app/shared/button/button.spec.tsx
+++ sport-app/shared/button/button.spec.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { axe, toHaveNoViolations } from 'jest-axe';
 import { Button } from './button';
 
+expect.extend(toHaveNoViolations);
+
 it('should render a button with the class of primary', () => {
-  render(<Button type={'primary'} label={'Primary'} />);
+  render(<Button type="primary" label="Primary" />);
   const primaryButton = screen.getByRole('button', { name: /primary/i });
   expect(primaryButton).toHaveClass('primary');
+});
+
+it('should have no basic accessibility issues', async () => {
+  const { container } = render(<Button type="primary" label="Primary" />);
+  expect(await axe(container)).toHaveNoViolations();
 });
